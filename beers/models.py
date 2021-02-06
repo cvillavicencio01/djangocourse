f58f96e60f4c0acfee588524c342afc8dd926991
Carlos Villavicencio <carlos.villavicencio@minds-lab.com>
2021-02-06 16:09:39 -0500

    str for brewery

--- beers/models.py
+++ beers/models.py
@@ -3,6 +3,8 @@ from django.db import models
 class Brewery(models.Model):
     name = models.CharField(max_length=250, blank= False, null=False)
 
+    def __str__(self):
+        return self.name
 
 class BeerType(models.Model):
     beer_type = models.CharField(max_length=250, blank= False, null=False)
