4bba30d498f406dbb42a74ae4351246f44f8b286
Carlos Villavicencio <carlos.villavicencio@minds-lab.com>
2021-02-14 18:17:34 -0500

    url namespace

--- beers/urls.py
+++ beers/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'beers'
+
 urlpatterns = [
     path('', views.brewery, name='brewery'),
     re_path(r'(?P<brewery_id>[0-9]+)/$', views.brewery_details, name='brewery_details'),
