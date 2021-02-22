452d4a9e601cad441f49b2c0a43fdd7aed9d3138
Carlos Villavicencio <carlos.villavicencio@minds-lab.com>
2021-02-21 19:04:23 -0500

    reorganizacion de las rutas y templates

--- beerweb/urls.py
+++ beerweb/urls.py
@@ -18,8 +18,10 @@ from django.urls import path
 
 from beers import views
 from django.conf.urls import include
+from . import views
 
 urlpatterns = [
+    path('', views.index),
     path('admin/', admin.site.urls),
     path('brewery/', include('beers.urls'))
 ]
